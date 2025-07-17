4f852de7ffd69fed76155df5df7b41bc249e2c21
FBEZ <francesco.bez@gmail.com>
2025-07-17 12:18:22 +0200

    added assignment 2_2 code

--- assignment_2_2/main/app_main.c
+++ assignment_2_2/main/app_main.c
@@ -4,10 +4,19 @@
 #include "esp_log.h"
 #include "esp_event.h"
 #include "esp_timer.h"
+#include "driver/gpio.h"
+#include "esp_attr.h"
 
 #define TEMPERATURE_MEAS_PERIOD_US (5 * 1000000)
 #define ALARM_CHECK_PERIOD_US      (200 * 1000)
 
+#define GPIO_INPUT_IO_9    9
+#define ESP_INTR_FLAG_DEFAULT 0
+
+
+static const char *TAG = "assignment2_2";
+
+// EVENTS
 ESP_EVENT_DEFINE_BASE(TEMP_EVENT_BASE);
 ESP_EVENT_DEFINE_BASE(ALARM_EVENT_BASE);
 
@@ -19,7 +28,18 @@ typedef enum {
 
 typedef enum {
     ALARM_EVENT_CHECK,
+    ALARM_EVENT_BUTTON
 } alarm_event_id_t;
+
+
+// GPIO
+
+// ISR handler
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    uint32_t gpio_num = (uint32_t) arg;
+    ESP_EARLY_LOGI(TAG, "GPIO[%d] interrupt triggered", gpio_num);
+    esp_event_isr_post(ALARM_EVENT_BASE, ALARM_EVENT_BUTTON, NULL, 0, 0);
+}
 
 static temperature_sensor_t *sensor = NULL;
 static alarm_t *alarm = NULL;
@@ -33,6 +53,14 @@ static void temp_event_handler(void* handler_arg, esp_event_base_t base, int32_t
         ESP_LOGW("APP", "Failed to read temperature");
     }
 }
+
+static void alarm_event_button_handler(void* handler_arg, esp_event_base_t base, int32_t id, void* event_data) {
+    
+        printf("ALARM ON!!\n");
+        cloud_manager_send_alarm(cloud);
+
+}
+
 
 static void alarm_event_handler(void* handler_arg, esp_event_base_t base, int32_t id, void* event_data) {
     
@@ -69,6 +97,7 @@ void app_main(void)
     // Register event handlers
     ESP_ERROR_CHECK(esp_event_handler_register(TEMP_EVENT_BASE, TEMP_EVENT_MEASURE, temp_event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(ALARM_EVENT_BASE, ALARM_EVENT_CHECK, alarm_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(ALARM_EVENT_BASE, ALARM_EVENT_BUTTON, alarm_event_button_handler, NULL));
 
     // Create and start periodic timers
     const esp_timer_create_args_t temp_timer_args = {
@@ -85,7 +114,23 @@ void app_main(void)
     };
     esp_timer_handle_t alarm_timer;
     ESP_ERROR_CHECK(esp_timer_create(&alarm_timer_args, &alarm_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(alarm_timer, ALARM_CHECK_PERIOD_US));
+    
+
+    // GPIO
+
+     gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_NEGEDGE,      // Falling edge interrupt
+        .mode = GPIO_MODE_INPUT,             // Set as input mode
+        .pin_bit_mask = (1ULL << GPIO_INPUT_IO_9),
+        .pull_up_en = GPIO_PULLUP_ENABLE,    // Enable pull-up
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    };
+    gpio_config(&io_conf);
+
+    // Install GPIO ISR service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    // Hook ISR handler for specific GPIO pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_9, gpio_isr_handler, (void*) GPIO_INPUT_IO_9);
 
     // The main task can now just sleep
     while (1) {
